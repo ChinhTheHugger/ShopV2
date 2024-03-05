1ec53d8c5dbdd9811e9bd95767118c47a22a28e6
Pham Minh Son <sonpm4@ghtk.co>
2024-03-05 08:09:45 +0700

    commit

--- car/middleware/auth.py
+++ car/middleware/auth.py
@@ -15,17 +15,17 @@ def auth_middleware(get_response):
 
     return middleware
 
-def auth_middleware_ad(get_response):
-    # One-time configuration and initialization.
+# def auth_middleware_ad(get_response):
+#     # One-time configuration and initialization.
 
-    def middleware_ad(request):
-        print(request.session.get('manager'))
-        returnUrl = request.META['PATH_INFO']
-        print(request.META['PATH_INFO'])
-        if not request.session.get('manager'):
-           return redirect(f'login?return_url={returnUrl}')
+#     def middleware_ad(request):
+#         print(request.session.get('manager'))
+#         returnUrl = request.META['PATH_INFO']
+#         print(request.META['PATH_INFO'])
+#         if not request.session.get('manager'):
+#            return redirect(f'login?return_url={returnUrl}')
 
-        response = get_response(request)
-        return response
+#         response = get_response(request)
+#         return response
 
-    return middleware_ad
+#     return middleware_ad
